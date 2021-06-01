e791d759d9fcec93d3b46a4b5f51fa57e16f0e3d
sarahdong <sarahdong@sarahs-air.attlocal.net>
2021-06-01 14:01:30 -0700

    chicken left right with keyboard working

--- Crossy Road Rip-off/src/Chicken.java
+++ Crossy Road Rip-off/src/Chicken.java
@@ -77,6 +77,7 @@ public class Chicken implements MouseListener{
 	}
 	public void left() {
 		x-=5;
+		y-=2;
 		vx=0;
 		//vy=-1;
 	init(x,y);
@@ -84,6 +85,7 @@ public class Chicken implements MouseListener{
 }
 	public void right() {
 		x+=5;
+		y+=2;
 		vx=0;
 		//vy=-1;
 	init(x,y);
